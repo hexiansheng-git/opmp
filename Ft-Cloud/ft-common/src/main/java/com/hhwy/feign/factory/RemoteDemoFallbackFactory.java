e1e7ac6b04bba39431228151a34d51fd4b265449
zhouguox <zhouguox>
2023-06-30 16:34:42 +0800

    补充

--- Ft-Cloud/ft-common/src/main/java/com/hhwy/feign/factory/RemoteDemoFallbackFactory.java
+++ Ft-Cloud/ft-common/src/main/java/com/hhwy/feign/factory/RemoteDemoFallbackFactory.java
@@ -1,7 +1,7 @@
 package com.hhwy.feign.factory;
 
 import com.hhwy.common.core.web.domain.AjaxResult;
-import com.hhwy.pm.api.RemoteDemoService;
+import com.hhwy.feign.service.RemoteDemoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FallbackFactory;
