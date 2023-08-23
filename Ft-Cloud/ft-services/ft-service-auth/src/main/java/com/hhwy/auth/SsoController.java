1b75d79bba17b3b2fdba854a23c558ca230eb797
wukun <wukun@ieforever.com>
2023-08-23 11:30:30 +0800

    sso单点接口加密

--- Ft-Cloud/ft-services/ft-service-auth/src/main/java/com/hhwy/auth/SsoController.java
+++ Ft-Cloud/ft-services/ft-service-auth/src/main/java/com/hhwy/auth/SsoController.java
@@ -2,20 +2,26 @@ package com.hhwy.auth;
 
 
 import com.hhwy.auth.core.service.SysLoginService;
+import com.hhwy.auth.utils.EncryptUtils;
 import com.hhwy.common.core.domain.R;
 import com.hhwy.common.core.utils.DESUtil;
 import com.hhwy.common.core.web.domain.AjaxResult;
 import com.hhwy.common.security.service.TokenService;
 import com.hhwy.system.api.model.LoginUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/pmsso")
+@RefreshScope(proxyMode = ScopedProxyMode.DEFAULT)
 public class SsoController {
-
+    @Value("${pm.sso.secrekey}")
+    private String ssoSecrekey;
     @Autowired
     private TokenService tokenService;
     @Autowired
@@ -25,6 +31,8 @@ public class SsoController {
     public R<?> ssoValidate(@RequestBody Map<String, String> params) {
         String tenantKey = (String)params.get("tenantKey");
         String username = (String)params.get("userName");
+        tenantKey = EncryptUtils.AESDecode(tenantKey, ssoSecrekey);
+        username = EncryptUtils.AESDecode(username, ssoSecrekey);
         LoginUser userInfo = this.sysLoginService.usernameValidate(tenantKey, username);
         return R.ok(this.tokenService.createToken(userInfo, tenantKey));
     }
