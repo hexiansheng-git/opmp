6d778a9db176799fce4cbb9f754b168911f70f1d
han <1027844116@qq.com>
2023-08-25 18:01:24 +0800

    2.5.5接口修改

--- Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/qqch/preparation/survey/optimize/domain/QqchComparisonSchemeContent.java
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/qqch/preparation/survey/optimize/domain/QqchComparisonSchemeContent.java
@@ -1,17 +1,17 @@
 package com.hhwy.pm.qqch.preparation.survey.optimize.domain;
 
-import com.hhwy.common.core.web.domain.BaseEntity;
-
-import java.math.BigDecimal;
-import java.util.Date;
-import com.hhwy.common.core.annotation.Excel;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.hhwy.common.core.annotation.Excel;
+import com.hhwy.common.core.web.domain.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * @author han
@@ -56,7 +56,13 @@ public class QqchComparisonSchemeContent extends BaseEntity {
      */
     @JsonProperty
     @Excel(name = "排序")
-    private String sort;
+    private Integer sort;
+    /**
+     * 字段描述：行号
+     */
+    @JsonProperty
+    @Excel(name = "行号")
+    private Integer rownum;
     /**
      * 字段描述：版本
      */
