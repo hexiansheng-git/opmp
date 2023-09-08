711760b1e17b3ab23b1b781ccf661f04e6f5bf79
w_zhenglili3 <1247538809@qq.com>
2023-09-08 17:05:23 +0800

    修改bug

--- Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/jdgl/diff/make/domain/JdglCorrectionMeasuresMakeDetail.java
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/jdgl/diff/make/domain/JdglCorrectionMeasuresMakeDetail.java
@@ -130,7 +130,7 @@ public class JdglCorrectionMeasuresMakeDetail extends TreeNode<JdglCorrectionMea
      */
     @JsonProperty
     @Excel(name = "纠偏完成日期")
-    private BigDecimal correctionCompleteDate;
+    private Date correctionCompleteDate;
     /**
      * 字段描述：作业队伍
      */
--- Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/jdgl/diff/track/domain/JdglProgressCorrectionTrackDetail.java
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/jdgl/diff/track/domain/JdglProgressCorrectionTrackDetail.java
@@ -130,7 +130,7 @@ public class JdglProgressCorrectionTrackDetail extends TreeNode<JdglProgressCorr
      */
     @JsonProperty
     @Excel(name = "纠偏完成日期")
-    private BigDecimal correctionCompleteDate;
+    private Date correctionCompleteDate;
     /**
      * 字段描述：作业队伍
      */
