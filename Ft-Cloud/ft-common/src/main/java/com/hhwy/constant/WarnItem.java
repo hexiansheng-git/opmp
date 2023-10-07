53f6b8d90f1fcd51ed03b541cd48d3864bf379c7
w_zhenglili3 <1247538809@qq.com>
2023-10-07 18:21:48 +0800

    解决冲突

--- Ft-Cloud/ft-common/src/main/java/com/hhwy/constant/WarnItem.java
+++ Ft-Cloud/ft-common/src/main/java/com/hhwy/constant/WarnItem.java
@@ -6,15 +6,11 @@ package com.hhwy.constant;
 public enum WarnItem {
 
 
-<<<<<<< HEAD
     WORK_GROUP_SET_UP("前期策划工作小组设立", "work_group_set_up", "项目中标之后的10天内，成立前期策划临时工作小组并完成审批，未完成则进行预警"),
     SUMMARY_SET_UP("前期策划总结评价-总结", "summary_set_up", "项目在完成初验后10天内报送前期策划总结，未报送则进行预警"),
-    EVALUATION_SET_UP("前期策划总结评价-评价", "evaluation_set_up", "名国家总项目部 (片区公司) 在项目报送前期策划总结后7天内进行评价，未完成进行预警");
-=======
-    WORK_GROUP_SET_UP("前期策划工作小组设立","work_group_set_up","项目中标之后的10天内，成立前期策划临时工作小组并完成审批，未完成则进行预警"),
+    EVALUATION_SET_UP("前期策划总结评价-评价", "evaluation_set_up", "名国家总项目部 (片区公司) 在项目报送前期策划总结后7天内进行评价，未完成进行预警"),
     WORK_PLAN_COMMIT("前期策划工作计划提交","work_plan_commit","应在前期策划临时工作小组审批之后的3天内，按时提交前期策划工作计划报请审批，未提交则进行预警"),
     WORK_PLAN_APPROVAL("前期策划工作计划审批","work_plan_approval","前期策划工作计划提交后，三天内完成审批，未完成则进行预警");
->>>>>>> 80366e17e66c8b74fdf77c68b8120762febc93e4
 
     public String getWarnItem() {
         return warnItem;
