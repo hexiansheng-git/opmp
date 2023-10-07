3846c1e278db073a4ecbf2f5f5fb147b15025110
han <1027844116@qq.com>
2023-10-07 11:16:16 +0800

    添加回滚类

---
+++ Ft-Cloud/ft-services/ft-service-job/src/main/java/com/hhwy/job/feign/factory/PmServiceFallbackFactory.java
@@ -0,0 +1,19 @@
+package com.hhwy.job.feign.factory;
+
+import com.hhwy.common.core.web.domain.AjaxResult;
+import com.hhwy.job.feign.service.PmServiceApi;
+import org.springframework.cloud.openfeign.FallbackFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PmServiceFallbackFactory implements FallbackFactory<PmServiceApi> {
+    @Override
+    public PmServiceApi create(Throwable cause) {
+        return new PmServiceApi() {
+            @Override
+            public AjaxResult workGroupSetUpWarn() {
+                return null;
+            }
+        };
+    }
+}
