117a64ea027c29b5f6dc49ecd6eb551a82c2cd01
han <1027844116@qq.com>
2023-10-09 17:26:07 +0800

    前期策划执行检查保存报错修改

--- Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/qqch/qqchPerformInspection/service/impl/QqchPerformInspectionServiceImpl.java
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/qqch/qqchPerformInspection/service/impl/QqchPerformInspectionServiceImpl.java
@@ -114,7 +114,7 @@ public class QqchPerformInspectionServiceImpl implements IQqchPerformInspectionS
         //修改主表
         int result = qqchPerformInspectionMapper.updateQqchPerformInspection(qqchPerformInspection);
         //若为发起，推送数据到总部
-        if(qqchPerformInspection.getPtVar5().equals("1")) {
+        if("1".equals(qqchPerformInspection.getPtVar5())) {
             sysSyncInfoService.pushQqchPerformInspection(qqchPerformInspection);
         }
         return result;
