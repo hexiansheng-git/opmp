7d799b3507ca8f0b77be5f13abb4eeb940b9afc6
zhouguox <>
2023-10-23 16:22:48 +0800

    错误包修改

--- Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/ehr/service/IEhrService.java
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/ehr/service/IEhrService.java
@@ -4,7 +4,6 @@ package com.hhwy.pm.ehr.service;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.rpc.ServiceException;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -17,7 +16,7 @@ public interface IEhrService {
      * @param userNameList   用户登录名称list
      * @return
      */
-    Map<String,Object>  getCertList(String userName4A) throws ServiceException, ParserConfigurationException, IOException, SAXException;
+    Map<String,Object>  getCertList(String userName4A) throws  ParserConfigurationException, IOException, SAXException;
     
 
 
