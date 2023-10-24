50b6a2666ec88b99acb4ed15b54a771e9e012931
han <1027844116@qq.com>
2023-10-24 15:02:39 +0800

    人资证件接口

---
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/ehr/domain/Attachment.java
@@ -0,0 +1,10 @@
+package com.hhwy.pm.ehr.domain;
+
+import lombok.Data;
+
+@Data
+public class Attachment {
+
+    private String name;
+    private String id;
+}
--- Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/ehr/service/IEhrService.java
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/ehr/service/IEhrService.java
@@ -1,10 +1,12 @@
 package com.hhwy.pm.ehr.service;
 
 
+import com.hhwy.pm.ehr.domain.PersonCertifyCompetency;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 
@@ -17,6 +19,5 @@ public interface IEhrService {
      */
     Map<String,Object>  getCertList(String userName4A) throws  ParserConfigurationException, IOException, SAXException;
 
-
-
+    Map<String, List<PersonCertifyCompetency>> getCertListByUserName4As(String userName4As) throws  ParserConfigurationException, IOException, SAXException;
 }
