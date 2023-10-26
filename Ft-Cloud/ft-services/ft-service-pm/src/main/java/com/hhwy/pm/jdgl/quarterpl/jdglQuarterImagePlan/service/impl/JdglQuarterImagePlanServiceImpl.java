0382f8ddf93fbe42c39e30b44e392688784c02d5
chenjinhao <13661326043@163.com>
2023-10-26 17:36:57 +0800

    进度统计形象进度代码提交

--- Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/jdgl/quarterpl/jdglQuarterImagePlan/service/impl/JdglQuarterImagePlanServiceImpl.java
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/jdgl/quarterpl/jdglQuarterImagePlan/service/impl/JdglQuarterImagePlanServiceImpl.java
@@ -304,6 +304,7 @@ public class JdglQuarterImagePlanServiceImpl implements IJdglQuarterImagePlanSer
                     }
                 }
             }
+            if(jdglQuarterImagePlan.getTotalCompQuantity() == null) jdglQuarterImagePlan.setTotalCompQuantity(BigDecimal.ZERO);
             if(jdglQuarterImagePlan.getRemainQuantity() == null) jdglQuarterImagePlan.setRemainQuantity(jdglQuarterImagePlan.getDesignQuantity());
             jdglQuarterImagePlan.setPlanStartDate(jdglMainPlanItem.getStartDate());
             jdglQuarterImagePlan.setPlanEndDate(jdglMainPlanItem.getFinishDate());
