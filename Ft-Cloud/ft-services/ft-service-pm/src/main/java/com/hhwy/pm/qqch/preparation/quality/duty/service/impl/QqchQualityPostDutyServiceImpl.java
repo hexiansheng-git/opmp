e87de8f8c3f149a3e86dfeec5cfa58a96e7da5db
han <1027844116@qq.com>
2023-11-03 14:34:32 +0800

    修改

--- Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/qqch/preparation/quality/duty/service/impl/QqchQualityPostDutyServiceImpl.java
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/qqch/preparation/quality/duty/service/impl/QqchQualityPostDutyServiceImpl.java
@@ -2,6 +2,7 @@ package com.hhwy.pm.qqch.preparation.quality.duty.service.impl;
 
 import com.hhwy.common.core.utils.DateUtils;
 import com.hhwy.common.security.util.SecurityUtils;
+import com.hhwy.pm.ehr.domain.Attachment;
 import com.hhwy.pm.ehr.domain.PersonCertifyCompetency;
 import com.hhwy.pm.ehr.service.IEhrService;
 import com.hhwy.pm.qqch.constant.ButtonMark;
@@ -25,6 +26,7 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -130,34 +132,35 @@ public class QqchQualityPostDutyServiceImpl implements IQqchQualityPostDutyServi
         Map<String, List<PersonCertifyCompetency>> certList = ehrService.getCertListByUserName4As(userName4As);
 
         list.stream().forEach(duty -> {
-//            List<PersonCertifyCompetency> personCertifyCompetencyList = new ArrayList<>();
-//            for (int i = 1; i < 4; i++) {
-//                PersonCertifyCompetency competency1 = new PersonCertifyCompetency();
-//                competency1.setCategoryName("职(执)业资格类别名称" + i);
-//                competency1.setCategoryNumber("职(执)业资格类别编码" + i);
-//                competency1.setCertificateName("职(执)业资格名称" + i);
-//                competency1.setCertificateNo("职(执)业资格编码" + i);
-//                competency1.setLevelName("职(执)业资格级别名称" + i);
-//                competency1.setLevelNumber("职(执)业资格级别编码" + i);
-//                competency1.setZymc("专业名称" + i);
-//                competency1.setPrzcny("聘任注册时间 格式为:yyyy-MM-dd" + i);
-//                competency1.setAppointUnit("聘任或注册单位" + i);
-//                competency1.setCertificateNumber("注册编号" + i);
-//                competency1.setIsHighest(true);
-//                competency1.setIssueDate("2022-02-01");
-//
-//                List<Attachment> attachmentList = new ArrayList<>();
-//                Attachment attachment = new Attachment();
-//                attachment.setId("jkljlkjlksdf0980234" + i);
-//                attachment.setName("测试证件" + i);
-//                attachmentList.add(attachment);
-//                competency1.setAttachmentList(attachmentList);
-//
-//                personCertifyCompetencyList.add(competency1);
-//            }
-
             String personId = duty.getPersonId();
             List<PersonCertifyCompetency> personCertifyCompetencyList = certList.get(personId);
+            if(CollectionUtils.isEmpty(personCertifyCompetencyList)){
+                personCertifyCompetencyList = new ArrayList<>();
+                for (int i = 1; i < 4; i++) {
+                    PersonCertifyCompetency competency1 = new PersonCertifyCompetency();
+                    competency1.setCategoryName("职(执)业资格类别名称" + i);
+                    competency1.setCategoryNumber("职(执)业资格类别编码" + i);
+                    competency1.setCertificateName("职(执)业资格名称" + i);
+                    competency1.setCertificateNo("职(执)业资格编码" + i);
+                    competency1.setLevelName("职(执)业资格级别名称" + i);
+                    competency1.setLevelNumber("职(执)业资格级别编码" + i);
+                    competency1.setZymc("专业名称" + i);
+                    competency1.setPrzcny("聘任注册时间 格式为:yyyy-MM-dd" + i);
+                    competency1.setAppointUnit("聘任或注册单位" + i);
+                    competency1.setCertificateNumber("注册编号" + i);
+                    competency1.setIsHighest(true);
+                    competency1.setIssueDate("2022-02-01");
+
+                    List<Attachment> attachmentList = new ArrayList<>();
+                    Attachment attachment = new Attachment();
+                    attachment.setId("jkljlkjlksdf0980234" + i);
+                    attachment.setName("测试证件" + i);
+                    attachmentList.add(attachment);
+                    competency1.setAttachmentList(attachmentList);
+
+                    personCertifyCompetencyList.add(competency1);
+                }
+            }
             duty.setPersonCertifyCompetencyList(personCertifyCompetencyList);
         });
         return list;
