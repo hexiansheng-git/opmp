56f345cebc57dac049a0ca85656cd57e8ba3132b
fushudong <fushudong@ctfo.com>
2023-11-07 09:59:17 +0800

    现场设备查询修改
    Change-Id: I5033bd748cf6bb3e4b9e9d7a34a4d57f53880e5c

--- Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/qqch/preparation/sbch/equAllot/service/impl/EquAllotServiceImpl.java
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/qqch/preparation/sbch/equAllot/service/impl/EquAllotServiceImpl.java
@@ -305,8 +305,8 @@ public class EquAllotServiceImpl implements EquAllotService {
 
     @Override
     public AjaxResult xzxcsb(ActiveEquVo activeEquVo) {
-//        String url = "http://10.11.238.63:10003/basic-api/fms/xcsb/xcsbMonthSelfEquInfo/list";
-        String url = "https://wms.cfhec.net/basic-api/fms/xcsb/xcsbMonthSelfEquInfo/list";
+        String url = "http://10.11.238.63:10003/basic-api/fms/xcsb/xcsbMonthSelfEquInfo/list";
+//        String url = "https://wms.cfhec.net/basic-api/fms/xcsb/xcsbMonthSelfEquInfo/list";
         AjaxResult ajaxResult;
         String tenantKey = SecurityUtils.getTenantKey();
         activeEquVo.setPrjCode(tenantKey);
