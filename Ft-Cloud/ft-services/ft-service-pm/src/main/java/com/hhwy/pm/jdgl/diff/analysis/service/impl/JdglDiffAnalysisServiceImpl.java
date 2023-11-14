99733d0dfb2fe745ca76dbeb2e19ae79c2465245
chenjinhao <13661326043@163.com>
2023-11-14 14:41:49 +0800

    差异化分析代码提交

--- Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/jdgl/diff/analysis/service/impl/JdglDiffAnalysisServiceImpl.java
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/jdgl/diff/analysis/service/impl/JdglDiffAnalysisServiceImpl.java
@@ -164,10 +164,10 @@ public class JdglDiffAnalysisServiceImpl implements IJdglDiffAnalysisService {
             if(!CollectionUtils.isEmpty(analyseList)) {
                 for (QqchScheAnalyse qqchScheAnalyse : analyseList) {
                     BigDecimal score = qqchScheAnalyse.getScore();
-                    BigDecimal sumMaxScore = qqchScheAnalyse.getSumMaxScore() == null ? BigDecimal.ZERO : qqchScheAnalyse.getSumMaxScore();
-                    BigDecimal sumMinScore = qqchScheAnalyse.getSumMinScore() == null ? BigDecimal.ZERO : qqchScheAnalyse.getSumMinScore();
+                    BigDecimal sumMaxScore = qqchScheAnalyse.getSumMaxScore();
+                    BigDecimal sumMinScore = qqchScheAnalyse.getSumMinScore();
 
-                    if(sumMin != null && sumMin.compareTo(sumMaxScore) < 0 && sumMin.compareTo(sumMinScore) >= 0) {
+                    if(sumMin != null && (sumMaxScore == null || sumMin.compareTo(sumMaxScore) < 0) && (sumMinScore == null || sumMin.compareTo(sumMinScore) >= 0)) {
                         jdglDiffAnalysis.setValueGrade(score);
                         countTotalGrade(jdglDiffAnalysis);
                     }
