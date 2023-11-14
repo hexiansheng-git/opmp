4855e46a33c5c80d099e1b863c502d65962f6887
han <1027844116@qq.com>
2023-11-14 11:55:08 +0800

    差异化分析接收数据，推送数据

--- Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/jdgl/diff/make/service/impl/JdglCorrectionMeasuresMakeServiceImpl.java
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/jdgl/diff/make/service/impl/JdglCorrectionMeasuresMakeServiceImpl.java
@@ -25,15 +25,16 @@ import com.hhwy.utils.core.DateUtil;
 import com.hhwy.utils.date.FtDateUtils;
 import com.hhwy.utils.idworker.IdWorker;
 import com.hhwy.utils.tree.TreeUtil;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import org.apache.commons.collections4.CollectionUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author zhenglili
@@ -244,8 +245,8 @@ public class JdglCorrectionMeasuresMakeServiceImpl implements IJdglCorrectionMea
         jdglCorrectionMeasuresMake.setWarnTime(FtDateUtils.getYearMonthDayDate());
         jdglCorrectionMeasuresMake.setRiskLevel(JdglDiffAnalysis.getRiskLevel());
         jdglCorrectionMeasuresMake.setPeriodTotalScore(JdglDiffAnalysis.getTotalGrade());
-        jdglCorrectionMeasuresMake.setCreateUser(String.valueOf(SecurityUtils.getUserId()));
-        jdglCorrectionMeasuresMake.setCreateUserName(SecurityUtils.getUserName());
+//        jdglCorrectionMeasuresMake.setCreateUser(String.valueOf(SecurityUtils.getUserId()));
+//        jdglCorrectionMeasuresMake.setCreateUserName(SecurityUtils.getUserName());
         jdglCorrectionMeasuresMake.setCreateTime(DateUtils.getNowDate());
         // 纠偏措施制定入库
         jdglCorrectionMeasuresMakeMapper.insertJdglCorrectionMeasuresMake(jdglCorrectionMeasuresMake);
