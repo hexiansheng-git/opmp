b88a31b342dc08b3a080cf380fee25d7cd38f204
wukun <wukun@ieforever.com>
2023-11-17 15:13:05 +0800

    前期策划变更版本号保留一位小数

--- Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/qqch/qqchChange/domain/QqchChange.java
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/qqch/qqchChange/domain/QqchChange.java
@@ -250,6 +250,6 @@ public class QqchChange extends CommonBaseEntity {
 
 
     public String getVersionStr() {
-        return version==null?null:"V"+version.setScale(0);
+        return version==null?null:"V"+version.setScale(1);
     }
 }
