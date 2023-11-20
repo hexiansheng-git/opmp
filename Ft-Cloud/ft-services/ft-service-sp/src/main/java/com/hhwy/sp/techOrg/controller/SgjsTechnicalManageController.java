fee90dcfb37e3e5c0e36c114164b780f0c2d47b2
liucaifang <liucaif@ieforever.com>
2023-11-20 10:01:03 +0800

    1、sp 施工技术初始化

---
+++ Ft-Cloud/ft-services/ft-service-sp/src/main/java/com/hhwy/sp/techOrg/controller/SgjsTechnicalManageController.java
@@ -0,0 +1,118 @@
+package com.hhwy.sp.techOrg.controller;
+
+import java.util.Arrays;
+import java.util.List;
+import java.io.IOException;
+
+import com.hhwy.sp.techOrg.domain.SgjsTechnicalManageVo;
+import org.springframework.web.bind.annotation.*;
+import javax.servlet.http.HttpServletResponse;
+import com.hhwy.common.core.utils.DateUtils;
+import com.hhwy.common.core.utils.poi.ExcelUtils;
+import com.hhwy.common.core.web.domain.AjaxResult;
+import com.hhwy.common.core.web.controller.BaseController;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.hhwy.sp.techOrg.service.ISgjsTechnicalManageService;
+import com.hhwy.sp.techOrg.domain.SgjsTechnicalManage;
+import org.springframework.validation.annotation.Validated;
+import com.hhwy.utils.validation.ValidationGroups;
+import com.hhwy.common.security.annotation.PreAuthorize;
+
+/**
+ * @author lcf
+ * @date 2023-11-17 11:29:23
+ * @remark
+ */
+@Validated
+@RestController
+@RequestMapping("/sgjsTechnicalManage")
+public class SgjsTechnicalManageController extends BaseController {
+
+    @Autowired
+    private ISgjsTechnicalManageService sgjsTechnicalManageService;
+
+    @PreAuthorize(hasPermi = "sgjsTechnicalManage:list")
+    @GetMapping
+    public AjaxResult getSgjsTechnicalManage(@Validated(ValidationGroups.Get.class) SgjsTechnicalManage sgjsTechnicalManageParam) {
+        SgjsTechnicalManage sgjsTechnicalManage = sgjsTechnicalManageService.getSgjsTechnicalManage(sgjsTechnicalManageParam);
+        return AjaxResult.success(sgjsTechnicalManage);
+    }
+
+    /**
+     * 列表
+     *
+     * @param sgjsTechnicalManageParam
+     * @return
+     */
+    @PreAuthorize(hasPermi = "sgjsTechnicalManage:list")
+    @GetMapping("/list")
+    public AjaxResult getSgjsTechnicalManageList(@Validated(ValidationGroups.Select.class) SgjsTechnicalManage sgjsTechnicalManageParam) {
+        SgjsTechnicalManageVo vo = sgjsTechnicalManageService.list(sgjsTechnicalManageParam);
+        return AjaxResult.success(vo);
+    }
+
+    /**
+     * 新增
+     *
+     * @param sgjsTechnicalManageParam
+     * @return
+     */
+    @PreAuthorize(hasPermi = "sgjsTechnicalManage:add")
+    @PostMapping("/add")
+    public AjaxResult insertSgjsTechnicalManage(@Validated(ValidationGroups.Save.class) @RequestBody SgjsTechnicalManage sgjsTechnicalManageParam) {
+        sgjsTechnicalManageService.insertSgjsTechnicalManage(sgjsTechnicalManageParam);
+        return AjaxResult.success();
+    }
+
+    @PreAuthorize(hasPermi = "sgjsTechnicalManage:add")
+    @PostMapping("/batchAdd")
+    public AjaxResult insertSgjsTechnicalManageList(@Validated(ValidationGroups.Save.class) @RequestBody SgjsTechnicalManageVo sgjsTechnicalManageVo) {
+        sgjsTechnicalManageService.batchAdd(sgjsTechnicalManageVo);
+        return AjaxResult.success();
+    }
+
+
+
+    /**
+     * 修改
+     *
+     * @param sgjsTechnicalManageParam
+     * @return
+     */
+    @PreAuthorize(hasPermi = "sgjsTechnicalManage:update")
+    @PostMapping("/update")
+    public AjaxResult updateSgjsTechnicalManage(@Validated(ValidationGroups.Update.class) @RequestBody SgjsTechnicalManage sgjsTechnicalManageParam) {
+        return toAjax(sgjsTechnicalManageService.updateSgjsTechnicalManage(sgjsTechnicalManageParam));
+    }
+
+    @PreAuthorize(hasPermi = "sgjsTechnicalManage:update")
+    @PostMapping("/batchUpdate")
+    public AjaxResult updateSgjsTechnicalManageList(@Validated(ValidationGroups.Update.class) @RequestBody List<SgjsTechnicalManage> sgjsTechnicalManageListParam) {
+        return toAjax(sgjsTechnicalManageService.updateSgjsTechnicalManageList(sgjsTechnicalManageListParam));
+    }
+
+    @PreAuthorize(hasPermi = "sgjsTechnicalManage:remove")
+    @PostMapping("/delete")
+    public AjaxResult deleteSgjsTechnicalManage(@Validated(ValidationGroups.Delete.class) @RequestBody SgjsTechnicalManage sgjsTechnicalManageParam) {
+        return toAjax(sgjsTechnicalManageService.deleteSgjsTechnicalManage(sgjsTechnicalManageParam));
+    }
+
+    /**
+     * 删除
+     *
+     * @param ids
+     * @return
+     */
+    @PreAuthorize(hasPermi = "sgjsTechnicalManage:remove")
+    @PostMapping("/del")
+    public AjaxResult deleteSgjsTechnicalManageByPks(@RequestBody List<Long> ids) {
+        return toAjax(sgjsTechnicalManageService.deleteSgjsTechnicalManageByPks(ids));
+    }
+
+    @GetMapping("/export")
+    public void export(HttpServletResponse response, SgjsTechnicalManage sgjsTechnicalManageParam) throws IOException {
+        List<SgjsTechnicalManage> sgjsTechnicalManageList = sgjsTechnicalManageService.getSgjsTechnicalManageList(sgjsTechnicalManageParam);
+        ExcelUtils<SgjsTechnicalManage> util = new ExcelUtils<>(SgjsTechnicalManage.class);
+        util.exportExcel(response, sgjsTechnicalManageList, DateUtils.getDate());
+    }
+}
---
+++ Ft-Cloud/ft-services/ft-service-sp/src/main/java/com/hhwy/sp/techOrg/mapper/SgjsTechnicalManageMapper.java
@@ -0,0 +1,43 @@
+package com.hhwy.sp.techOrg.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import com.hhwy.sp.techOrg.domain.SgjsTechnicalManage;
+
+/**
+ * @author lcf
+ * @date 2023-11-17 11:29:23
+ * @remark
+ */
+public interface SgjsTechnicalManageMapper {
+
+    SgjsTechnicalManage getSgjsTechnicalManage(SgjsTechnicalManage sgjsTechnicalManage);
+
+    List<SgjsTechnicalManage> getSgjsTechnicalManageList(SgjsTechnicalManage sgjsTechnicalManage);
+
+    int insertSgjsTechnicalManage(SgjsTechnicalManage sgjsTechnicalManage);
+
+    int insertSgjsTechnicalManageList(@Param("sgjsTechnicalManageList") List<SgjsTechnicalManage> sgjsTechnicalManageList);
+
+    int updateSgjsTechnicalManage(SgjsTechnicalManage sgjsTechnicalManage);
+
+    int updateSgjsTechnicalManageList(@Param("sgjsTechnicalManageList") List<SgjsTechnicalManage> sgjsTechnicalManageList);
+
+    int deleteSgjsTechnicalManage(SgjsTechnicalManage sgjsTechnicalManage);
+
+    int deleteSgjsTechnicalManageByPks(@Param("sgjsTechnicalManagePkList") List<Long> sgjsTechnicalManagePkList);
+
+    /**
+     * 批量查询根据id
+     * 有子节点 返回子节点信息
+     *
+     * @param sgjsTechnicalManagePkList
+     */
+    List<SgjsTechnicalManage> batchSelect(List<Long> sgjsTechnicalManagePkList);
+
+    /**
+     * 删除所有数据
+     *
+     */
+    void delectAll(SgjsTechnicalManage info);
+}
