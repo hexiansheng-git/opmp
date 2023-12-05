73bcc7c22ce395f9af3a27375b915faee8b97064
fushudong <fushudong@ctfo.com>
2023-12-05 12:59:40 +0800

    经营知识库-合同通用条件接口开发
    Change-Id: I4c961dd79a0424ea3089a0c20f4b4a9e7ad39649

---
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/qyzs/manage/qyzsManageContCondition/domain/QyzsManageContCondition4Update.java
@@ -0,0 +1,13 @@
+package com.hhwy.pm.qyzs.manage.qyzsManageContCondition.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class QyzsManageContCondition4Update {
+
+    List<QyzsManageContCondition> qyzsManageContConditionListParam;
+    String contVersion;
+
+}
