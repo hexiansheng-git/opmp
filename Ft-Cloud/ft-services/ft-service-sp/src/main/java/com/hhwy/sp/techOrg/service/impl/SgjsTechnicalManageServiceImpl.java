a4ffeff3b2928e5129c25628cdcbdd2a4bcbdc48
liucaifang <liucaif@ieforever.com>
2023-12-07 15:40:59 +0800

    同步总部数据接口放开

--- Ft-Cloud/ft-services/ft-service-sp/src/main/java/com/hhwy/sp/techOrg/service/impl/SgjsTechnicalManageServiceImpl.java
+++ Ft-Cloud/ft-services/ft-service-sp/src/main/java/com/hhwy/sp/techOrg/service/impl/SgjsTechnicalManageServiceImpl.java
@@ -220,7 +220,7 @@ public class SgjsTechnicalManageServiceImpl implements ISgjsTechnicalManageServi
             logger.info("子表未删除。。。。。。。。。。");
         }
         //同步总部数据
-        //syncDataToGm(treeToList);
+        syncDataToGm(treeToList);
         return AjaxResult.success();
     }
 
