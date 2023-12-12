c14ab209e22aa2fe9b35585af01abbfc3b500a80
zhaimanhang <782027052@qq.com>
2023-12-12 11:40:26 +0800

    导出调整

--- Ft-Cloud/ft-services/ft-service-sp/src/main/java/com/hhwy/sp/sgjsMeasure/sgjsReportMeasureSubmit/domain/SgjsReportMeasureSubmit.java
+++ Ft-Cloud/ft-services/ft-service-sp/src/main/java/com/hhwy/sp/sgjsMeasure/sgjsReportMeasureSubmit/domain/SgjsReportMeasureSubmit.java
@@ -35,7 +35,7 @@ public class SgjsReportMeasureSubmit extends TreeNode<SgjsReportMeasureSubmit> {
      */
     @JsonSerialize(using = ToStringSerializer.class)
     @JsonProperty
-    @Excel(name = "父级id")
+    //@Excel(name = "父级id")
     private Long pid;
     /**
      * 字段描述：报告名称
@@ -47,7 +47,7 @@ public class SgjsReportMeasureSubmit extends TreeNode<SgjsReportMeasureSubmit> {
      * 字段描述：所属wbs编号
      */
     @JsonProperty
-    @Excel(name = "所属wbs编号")
+    //@Excel(name = "所属wbs编号")
     private String wbsCode;
     /**
      * 字段描述：所属wbs名称
@@ -60,13 +60,13 @@ public class SgjsReportMeasureSubmit extends TreeNode<SgjsReportMeasureSubmit> {
      */
     @JsonSerialize(using = ToStringSerializer.class)
     @JsonProperty
-    @Excel(name = "所属WBS的id")
+    //@Excel(name = "所属WBS的id")
     private Long wbsId;
     /**
      * 字段描述：所属wbs祖级ID
      */
     @JsonProperty
-    @Excel(name = "所属wbs祖级ID")
+    //@Excel(name = "所属wbs祖级ID")
     private String wbsAncestors;
     /**
      * 字段描述：计划提交日期
@@ -113,7 +113,7 @@ public class SgjsReportMeasureSubmit extends TreeNode<SgjsReportMeasureSubmit> {
      * 字段描述：附件组id
      */
     @JsonProperty
-    @Excel(name = "附件组id")
+    //@Excel(name = "附件组id")
     private String fileGroupId;
     /**
      * 字段描述：备注
@@ -125,88 +125,88 @@ public class SgjsReportMeasureSubmit extends TreeNode<SgjsReportMeasureSubmit> {
      * 字段描述：数据来源 0新增1同步
      */
     @JsonProperty
-    @Excel(name = "数据来源 0新增1同步")
+    //@Excel(name = "数据来源 0新增1同步")
     private String dataSource;
     /**
      * 字段描述：数据创建者id
      */
     @JsonProperty
-    @Excel(name = "数据创建者id")
+    //@Excel(name = "数据创建者id")
     private String createUser;
     /**
      * 字段描述：数据创建者名称
      */
     @JsonProperty
-    @Excel(name = "数据创建者名称")
+    //@Excel(name = "数据创建者名称")
     private String createUserName;
     /**
      * 字段描述：数据创建系统时间
      */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonProperty
-    @Excel(name = "数据创建系统时间", dateFormat = "yyyy-MM-dd HH:mm:ss")
+    //@Excel(name = "数据创建系统时间", dateFormat = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
     /**
      * 字段描述：数据修改者id
      */
     @JsonProperty
-    @Excel(name = "数据修改者id")
+    //@Excel(name = "数据修改者id")
     private String updateUser;
     /**
      * 字段描述：数据修改系统时间
      */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonProperty
-    @Excel(name = "数据修改系统时间", dateFormat = "yyyy-MM-dd HH:mm:ss")
+    //@Excel(name = "数据修改系统时间", dateFormat = "yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
     /**
      * 字段描述：数据删除者
      */
     @JsonProperty
-    @Excel(name = "数据删除者")
+    //@Excel(name = "数据删除者")
     private String delUser;
     /**
      * 字段描述：数据删除系统时间
      */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonProperty
-    @Excel(name = "数据删除系统时间", dateFormat = "yyyy-MM-dd HH:mm:ss")
+    //@Excel(name = "数据删除系统时间", dateFormat = "yyyy-MM-dd HH:mm:ss")
     private Date delTime;
     /**
      * 字段描述：删除标识：0未删除；1已删除
      */
     @JsonProperty
-    @Excel(name = "删除标识：0未删除；1已删除")
+    //@Excel(name = "删除标识：0未删除；1已删除")
     private String delFlag;
     /**
      * 字段描述：预留字段1   项目编码
      */
     @JsonProperty
-    @Excel(name = "预留字段1   项目编码")
+    //@Excel(name = "预留字段1   项目编码")
     private String ptVar1;
     /**
      * 字段描述：预留字段2  leaf 是否是叶子节点 0否1是
      */
     @JsonProperty
-    @Excel(name = "预留字段2  leaf 是否是叶子节点 0否1是")
+    //@Excel(name = "预留字段2  leaf 是否是叶子节点 0否1是")
     private String ptVar2;
     /**
      * 字段描述：预留字段3
      */
     @JsonProperty
-    @Excel(name = "预留字段3")
+    //@Excel(name = "预留字段3")
     private String ptVar3;
     /**
      * 字段描述：预留字段4
      */
     @JsonProperty
-    @Excel(name = "预留字段4")
+    //@Excel(name = "预留字段4")
     private String ptVar4;
     /**
      * 字段描述：预留字段5
      */
     @JsonProperty
-    @Excel(name = "预留字段5")
+    //@Excel(name = "预留字段5")
     private String ptVar5;
 
     @JsonIgnore
