bcc84339ecc33d76f444b89750e0363f299e8d2b
han <1027844116@qq.com>
2023-12-14 17:34:07 +0800

    修改设置有效合同金额

--- Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/xmsl/contractInfo/service/impl/XmslContractInfoServiceImpl.java
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/xmsl/contractInfo/service/impl/XmslContractInfoServiceImpl.java
@@ -168,7 +168,11 @@ public class XmslContractInfoServiceImpl implements IXmslContractInfoService {
     @Override
     public void setEffectiveAmountDollar(XmslContractInfo contractInfo){
         String listCurrencyCode = contractInfo.getListCurrencyCode();
-        if("USD".equals(listCurrencyCode) || StringUtils.isBlank(listCurrencyCode)){
+        if(StringUtils.isBlank(listCurrencyCode)){
+            return;
+        }
+        if("USD".equals(listCurrencyCode)){
+            contractInfo.setEffectiveAmountDollar(contractInfo.getEffectiveAmout());
             return;
         }
         /*汇率*/
