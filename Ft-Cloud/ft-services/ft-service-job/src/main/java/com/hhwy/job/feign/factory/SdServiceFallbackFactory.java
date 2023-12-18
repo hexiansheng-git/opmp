277b3c477eabc366f7447294b938b1f0a4e56d77
han <1027844116@qq.com>
2023-12-18 11:49:32 +0800

    计划进度管理-月报定时任务

---
+++ Ft-Cloud/ft-services/ft-service-job/src/main/java/com/hhwy/job/feign/factory/SdServiceFallbackFactory.java
@@ -0,0 +1,21 @@
+package com.hhwy.job.feign.factory;
+
+import com.hhwy.common.core.web.domain.AjaxResult;
+import com.hhwy.job.feign.service.SdServiceApi;
+import org.springframework.cloud.openfeign.FallbackFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SdServiceFallbackFactory implements FallbackFactory<SdServiceApi> {
+
+    @Override
+    public SdServiceApi create(Throwable cause) {
+        return new SdServiceApi() {
+
+            @Override
+            public AjaxResult generateMonthlyReport() {
+                return null;
+            }
+        };
+    }
+}
