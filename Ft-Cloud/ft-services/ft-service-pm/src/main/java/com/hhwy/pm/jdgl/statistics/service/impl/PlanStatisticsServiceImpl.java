613d4f6d408c0a54ea63df0c9ae0f7c1790d547e
chenjinhao <13661326043@163.com>
2023-12-21 16:12:53 +0800

    条件查询不生效问题

--- Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/jdgl/statistics/service/impl/PlanStatisticsServiceImpl.java
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/jdgl/statistics/service/impl/PlanStatisticsServiceImpl.java
@@ -454,7 +454,7 @@ public class PlanStatisticsServiceImpl implements IPlanStatisticsService {
         List<JdglDayScheduleBill> billValueListByRangeDate = jdglDayScheduleBillService.getBillValueListByRangeDate(startDate, endDate);
 
         // 获取开累填报清单数据
-        List<JdglDayScheduleBill> billValueListByEndDate = jdglDayScheduleBillService.getBillValueListByEndDate(startDate);
+//        List<JdglDayScheduleBill> billValueListByEndDate = jdglDayScheduleBillService.getBillValueListByEndDate(startDate);
 
         if(CollectionUtils.isEmpty(billValueListByRangeDate)) {
             return returnList;
@@ -499,7 +499,7 @@ public class PlanStatisticsServiceImpl implements IPlanStatisticsService {
         }
 
         if(CollectionUtils.isEmpty(returnList)) {
-            return returnList;
+//            return returnList;
         }
 
         List<JdglWeekValuePlan> jdglWeekValuePlans = "z".equals(queryDateType) ? jdglWeekValuePlanService.getBillListByYearAndWeek(year, week) : null;
