0878f8008913b50a0263f535ad7959220574053e
chenjinhao <13661326043@163.com>
2023-12-21 16:20:20 +0800

    条件查询不生效问题

--- Ft-Cloud/ft-services/ft-service-sd/src/main/java/com/hhwy/sd/organManage/domain/KcsjOrganManage.java
+++ Ft-Cloud/ft-services/ft-service-sd/src/main/java/com/hhwy/sd/organManage/domain/KcsjOrganManage.java
@@ -103,7 +103,7 @@ public class KcsjOrganManage extends TreeNode<KcsjOrganManage> {
     /**
      * 字段描述：实际进场日期
      */
-    @JsonFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy年MM月dd日")
     @JsonProperty
     @Excel(name = "实际进场日期", dateFormat = "yyyy年MM月dd日")
     private Date actualEnterDate;
@@ -116,7 +116,7 @@ public class KcsjOrganManage extends TreeNode<KcsjOrganManage> {
     /**
      * 字段描述：实际退场日期
      */
-    @JsonFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy年MM月dd日")
     @JsonProperty
     @Excel(name = "实际退场日期", dateFormat = "yyyy年MM月dd日")
     private Date actualExitDate;
