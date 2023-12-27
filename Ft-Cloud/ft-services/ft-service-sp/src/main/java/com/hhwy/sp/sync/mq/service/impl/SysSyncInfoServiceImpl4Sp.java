964afcfc43a0edb76a1de89cbf877a2e0af3fba6
chenjinhao <13661326043@163.com>
2023-12-27 10:19:25 +0800

    交底记录同步数据接口增加区域字段

--- Ft-Cloud/ft-services/ft-service-sp/src/main/java/com/hhwy/sp/sync/mq/service/impl/SysSyncInfoServiceImpl4Sp.java
+++ Ft-Cloud/ft-services/ft-service-sp/src/main/java/com/hhwy/sp/sync/mq/service/impl/SysSyncInfoServiceImpl4Sp.java
@@ -2,6 +2,7 @@ package com.hhwy.sp.sync.mq.service.impl;
 
 import com.alibaba.fastjson.JSONObject;
 import com.hhwy.common.security.util.SecurityUtils;
+import com.hhwy.feign.service.PmServiceApi;
 import com.hhwy.sp.sgjsDiscloseRecord.domain.SgjsDiscloseRecord;
 import com.hhwy.sp.sync.mq.service.ISysSyncInfoService4Sp;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 数据同步节点记录Service业务层处理
@@ -22,13 +24,20 @@ public class SysSyncInfoServiceImpl4Sp implements ISysSyncInfoService4Sp {
     @Autowired
     private RocketMQTemplate rocketMQTemplate;
 
+    @Autowired
+    private PmServiceApi pmServiceApi;
+
     @Override
     @Transactional
     public void pushSgjsDiscloseRecord(List<SgjsDiscloseRecord> list) {
-        try{List<JSONObject> finalList = new ArrayList<>();
+        try{
+            List<JSONObject> finalList = new ArrayList<>();
+            Map<String, Object> prjInfo = pmServiceApi.getPrjInfo();
             for (int i = 0; i < list.size(); i++) {
                 SgjsDiscloseRecord temp = list.get(i);
                 temp.setPtVar2(SecurityUtils.getTenantKey());
+                temp.setRegionId((Long) prjInfo.get("regionId"));
+                temp.setRegionName((String) prjInfo.get("regionName"));
                 JSONObject json = JSONObject.parseObject(JSONObject.toJSONString(list.get(i)));
                 finalList.add(json);
             }
