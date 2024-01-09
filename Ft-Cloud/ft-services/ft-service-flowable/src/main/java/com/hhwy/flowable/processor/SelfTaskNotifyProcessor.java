172a74835b547af231a814bd136baeb8faf7440e
jinzhaoqiang <981422619@qq.com>
2024-01-09 14:06:36 +0800

    待办通知

---
+++ Ft-Cloud/ft-services/ft-service-flowable/src/main/java/com/hhwy/flowable/processor/SelfTaskNotifyProcessor.java
@@ -0,0 +1,35 @@
+package com.hhwy.flowable.processor;
+
+import com.hhwy.common.core.utils.http.HttpUtils;
+import com.hhwy.flowable.core.processor.TaskNotifyProcessor;
+import com.hhwy.system.api.RemoteNotifyService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SelfTaskNotifyProcessor implements TaskNotifyProcessor {
+
+    @Autowired
+    private RemoteNotifyService notifyService;
+
+    @Value("${socketio.remote.systemServer.uri}") //网关转发系统服务："http://127.0.0.1:8086/system" 或 直连系统服务："http://127.0.0.1:9300"
+    String remoteSystemServerUri;
+
+    @Override
+    public void publish(String clientId, String topic, String message) {
+        notifyService.publish(clientId,topic,message);
+        if(StringUtils.isNotEmpty(remoteSystemServerUri)){
+            String url = remoteSystemServerUri + "/notify/publish/" + clientId;
+            String param = "topic="+topic+"&message="+message;
+            try {
+                String result = HttpUtils.sendGet(url, param);
+                System.out.println("---notify result---:"+result);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
