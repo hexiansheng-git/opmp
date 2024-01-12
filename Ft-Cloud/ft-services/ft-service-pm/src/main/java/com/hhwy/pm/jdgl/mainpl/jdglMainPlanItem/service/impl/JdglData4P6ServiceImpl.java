7f2eb50daf7317aaba1f7b5fb7d2ef212dbce017
chenjinhao <13661326043@163.com>
2024-01-12 09:04:22 +0800

    p6接口区分处理

--- Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/jdgl/mainpl/jdglMainPlanItem/service/impl/JdglData4P6ServiceImpl.java
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/jdgl/mainpl/jdglMainPlanItem/service/impl/JdglData4P6ServiceImpl.java
@@ -675,7 +675,7 @@ public class JdglData4P6ServiceImpl implements IJdglData4P6Service {
      * @return
      */
     public ProjectInfo getProjectInfo(String projectCode) {
-        String urlProj = p6IpPort + pre + "/projectInfo";
+        String urlProj = p6IpPort + pre + "/projectInfoRealTime";
         ParameterizedTypeReference<List<ProjectInfo>> responseType4Proj = new ParameterizedTypeReference<List<ProjectInfo>>() {
         };
         HttpEntity<?> entity = new HttpEntity(new HttpHeaders());
