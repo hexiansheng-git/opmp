4a620b4df153c151dcefe4facff2b49d95aaebc5
chenjinhao <13661326043@163.com>
2024-02-20 14:06:21 +0800

    p6数据没有日期问题

--- Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/jdgl/mainpl/jdglMainPlanItem/service/impl/JdglMainPlanItemServiceImpl.java
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/jdgl/mainpl/jdglMainPlanItem/service/impl/JdglMainPlanItemServiceImpl.java
@@ -215,7 +215,8 @@ public class JdglMainPlanItemServiceImpl implements IJdglMainPlanItemService {
                 // 如果已经有实际开始时间，则取实际开始时间，否则取尚需最早开始;
                 Date start_date = jdglMainPlanItem1.getActualStartDate() != null
                         ? jdglMainPlanItem1.getActualStartDate() : jdglMainPlanItem1.getRemainingEarlyStartDate();
-                jdglMainPlanItem1.setStart_date(start_date);
+                jdglMainPlanItem1.setStart_date(null == start_date ?
+                        DateUtils.parseDate("0000-00-00") : start_date);
 
                 // 如果已经有实际完成时间，则取实际完成时间，否则取尚需最早完成;
                 Date end_date = jdglMainPlanItem1.getActualFinishDate() != null
