75ed51c6ec2255c8b477cb2bda31ef3f0bb71107
han <1027844116@qq.com>
2024-03-05 17:28:34 +0800

    论文导出修改

--- Ft-Cloud/ft-services/ft-service-sp/src/main/java/com/hhwy/sp/techManagement/sgjsPaperPublish/controller/SgjsPaperPublishController.java
+++ Ft-Cloud/ft-services/ft-service-sp/src/main/java/com/hhwy/sp/techManagement/sgjsPaperPublish/controller/SgjsPaperPublishController.java
@@ -143,7 +143,7 @@ public class SgjsPaperPublishController extends BaseController {
     public void export(HttpServletResponse response,@RequestBody PaperPublishQueryVo queryVo) throws IOException {
         List<Long> ids = queryVo.getIds();
         List<SgjsPaperPublish> sgjsPaperPublishList;
-        if(CollectionUtils.isNotEmpty(ids)){
+        if(CollectionUtils.isEmpty(ids)){
             sgjsPaperPublishList = sgjsPaperPublishService.getSgjsPaperPublishList(queryVo);
         }else {
             sgjsPaperPublishList = sgjsPaperPublishService.getListByIds(ids);
