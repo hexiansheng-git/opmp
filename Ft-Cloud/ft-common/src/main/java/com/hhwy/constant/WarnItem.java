e073fdd86168f7dec35de24f849ef999184be564
liucaifang <liucaif@ieforever.com>
2024-04-02 10:31:04 +0800

    漏提

--- Ft-Cloud/ft-common/src/main/java/com/hhwy/constant/WarnItem.java
+++ Ft-Cloud/ft-common/src/main/java/com/hhwy/constant/WarnItem.java
@@ -21,7 +21,8 @@ public enum WarnItem {
     PERSON_CONTROL_PLAN_ONE("人员管控策划", "person_control_plan_one", "证件到期前1个月，进行预警"),
     PERSON_CONTROL_PLAN_TWO("人员管控策划", "person_control_plan_two", "证件到期前2个月，进行预警"),
     PERSON_CONTROL_PLAN_THREE("人员管控策划", "person_control_plan_three", "证件到期前3个月，进行预警"),
-    WBS_P6_WARN("P6推送预警", "wbs_p6_warn", "P6数据已推送，请及时上传作业");
+    WBS_P6_WARN("P6推送预警", "wbs_p6_warn", "P6数据已推送，请及时上传作业"),
+    KCSJ_PLAN_PROCESS("勘察设计-计划进度","plan_process_warn","提前七天提醒一次给项目总工；超期每两天提醒预警人");
 
     public String getWarnItem() {
         return warnItem;
