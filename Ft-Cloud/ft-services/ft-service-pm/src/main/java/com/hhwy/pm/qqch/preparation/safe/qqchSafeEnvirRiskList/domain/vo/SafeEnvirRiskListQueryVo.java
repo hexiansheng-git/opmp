9b16769171a440bda685b52364c665e234d951f8
han <1027844116@qq.com>
2024-04-23 14:16:32 +0800

    bug修改

--- Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/qqch/preparation/safe/qqchSafeEnvirRiskList/domain/vo/SafeEnvirRiskListQueryVo.java
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/qqch/preparation/safe/qqchSafeEnvirRiskList/domain/vo/SafeEnvirRiskListQueryVo.java
@@ -17,7 +17,7 @@ public class SafeEnvirRiskListQueryVo {
     @NotBlank(message = "wbsId不能为空！",groups = ValidationGroups.Select.class)
     private String wbsId;
 
-    @NotBlank(message = "wbsI编码不能为空！",groups = ValidationGroups.Select.class)
+    @NotBlank(message = "wbs编码不能为空！",groups = ValidationGroups.Select.class)
     private String wbsCode;
 
     private BigDecimal version;
