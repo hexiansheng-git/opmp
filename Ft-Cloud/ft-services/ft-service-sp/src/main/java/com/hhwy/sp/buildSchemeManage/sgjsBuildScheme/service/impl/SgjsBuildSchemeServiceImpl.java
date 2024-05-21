33dd0ab8673ccb4bf9d71557013eb91232f96abe
fushudong <10413614@qq.com>
2024-05-21 16:24:53 +0800

    方案清单 调整时不需要附件
    Change-Id: I5ffebac06815bfa4f189a9f49374569fb7e68d37

--- Ft-Cloud/ft-services/ft-service-sp/src/main/java/com/hhwy/sp/buildSchemeManage/sgjsBuildScheme/service/impl/SgjsBuildSchemeServiceImpl.java
+++ Ft-Cloud/ft-services/ft-service-sp/src/main/java/com/hhwy/sp/buildSchemeManage/sgjsBuildScheme/service/impl/SgjsBuildSchemeServiceImpl.java
@@ -243,18 +243,18 @@ public class SgjsBuildSchemeServiceImpl implements ISgjsBuildSchemeService {
         SgjsBuildScheme validVersionData = sgjsBuildSchemeMapper.getValidVersionData();
         if (validVersionData != null ) result.setPtVar5(String.valueOf(validVersionData.getId()));
         //附件组id更新
-        String auditRecordFile = lastData.getAuditRecordFile();
-        String auditRecordFileRegion = lastData.getAuditRecordFileRegion();
-        String projectSummaryFile = lastData.getProjectSummaryFile();
-        if (StringUtils.isNotEmpty(auditRecordFile)) {
-            result.setAuditRecordFile(fileUploadUtil.copyFile(auditRecordFile));
-        }
-        if (StringUtils.isNotEmpty(auditRecordFileRegion)) {
-            result.setAuditRecordFile(fileUploadUtil.copyFile(auditRecordFileRegion));
-        }
-        if (StringUtils.isNotEmpty(projectSummaryFile)) {
-            result.setProjectSummaryFile(fileUploadUtil.copyFile(projectSummaryFile));
-        }
+//        String auditRecordFile = lastData.getAuditRecordFile();
+//        String auditRecordFileRegion = lastData.getAuditRecordFileRegion();
+//        String projectSummaryFile = lastData.getProjectSummaryFile();
+//        if (StringUtils.isNotEmpty(auditRecordFile)) {
+//            result.setAuditRecordFile(fileUploadUtil.copyFile(auditRecordFile));
+//        }
+//        if (StringUtils.isNotEmpty(auditRecordFileRegion)) {
+//            result.setAuditRecordFile(fileUploadUtil.copyFile(auditRecordFileRegion));
+//        }
+//        if (StringUtils.isNotEmpty(projectSummaryFile)) {
+//            result.setProjectSummaryFile(fileUploadUtil.copyFile(projectSummaryFile));
+//        }
         return result;
     }
 
