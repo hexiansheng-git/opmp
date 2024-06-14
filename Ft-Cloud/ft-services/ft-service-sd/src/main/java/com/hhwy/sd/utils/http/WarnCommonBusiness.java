07a4093914b6797d64a95d1fcd271ea3234f63ac
liucaifang <liucaif@ieforever.com>
2024-06-14 16:23:12 +0800

    注入坑

--- Ft-Cloud/ft-services/ft-service-sd/src/main/java/com/hhwy/sd/utils/http/WarnCommonBusiness.java
+++ Ft-Cloud/ft-services/ft-service-sd/src/main/java/com/hhwy/sd/utils/http/WarnCommonBusiness.java
@@ -10,7 +10,6 @@ import com.hhwy.feign.service.SystemServiceApi;
 import com.hhwy.sd.planProcess.kcsjPlanProcess.domain.KcsjWarnConfig;
 import com.hhwy.system.api.domain.SysUser;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.util.Assert;
 import org.springframework.util.MultiValueMap;
 
@@ -45,8 +44,8 @@ public class WarnCommonBusiness {
      * 时间: 2024/4/2
      */
     public static KcsjWarnConfig getSgjsWarnConfig(String url, String warnSubject) {
-        HttpHeaders headers = HttpHeadersUtils.getCommonHeaders();
-        HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(headers);
+        //HttpHeaders headers = HttpHeadersUtils.getCommonHeaders();
+        HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(null);
         AjaxResult ajaxResul = RestTemplateUtils.get(url, httpEntity, AjaxResult.class, warnSubject);
         Integer code = (Integer) ajaxResul.get("code");
         Assert.isTrue(code.equals(200), "从总部获取预警配置信息失败");
