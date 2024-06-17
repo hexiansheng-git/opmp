c4ee899f3ded786d352cac703f7eef6793fa302f
wukun <wukun@ieforever.com>
2024-06-17 11:04:44 +0800

    3.3.2岗位设置增加项目id、项目编号字段

--- Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/qqch/preparation/technique/manage/controller/QqchPostSettingController.java
+++ Ft-Cloud/ft-services/ft-service-pm/src/main/java/com/hhwy/pm/qqch/preparation/technique/manage/controller/QqchPostSettingController.java
@@ -5,9 +5,13 @@ import com.hhwy.common.core.web.domain.AjaxResult;
 import com.hhwy.pm.qqch.preparation.technique.manage.domain.QqchPostSetting;
 import com.hhwy.pm.qqch.preparation.technique.manage.domain.vo.QqchPostSettingVo;
 import com.hhwy.pm.qqch.preparation.technique.manage.service.IQqchPostSettingService;
+import com.hhwy.pm.xmsl.project.domain.XmslProjectBasicInfo;
+import com.hhwy.pm.xmsl.project.domain.vo.ProjectBasicInfo;
+import com.hhwy.pm.xmsl.project.service.IXmslProjectBasicInfoService;
 import com.hhwy.utils.customLog.CustomBusinessType;
 import com.hhwy.utils.customLog.CustomLogger;
 import com.hhwy.utils.validation.ValidationGroups;
+import org.ehcache.shadow.org.terracotta.offheapstore.storage.listener.ListenableStorageEngine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -27,6 +31,8 @@ public class QqchPostSettingController extends BaseController {
 
     @Autowired
     private IQqchPostSettingService qqchPostSettingService;
+    @Autowired
+    private IXmslProjectBasicInfoService projectBasicInfoService;
 
     @GetMapping("/getTreeList")
     public AjaxResult getTreeList(BigDecimal version) {
@@ -49,6 +55,13 @@ public class QqchPostSettingController extends BaseController {
      */
     @GetMapping("getTechDeptList")
     public List<QqchPostSetting> getTechDeptList() {
-        return qqchPostSettingService.getTechDeptList();
+        ProjectBasicInfo projectInfo = projectBasicInfoService.projectInfo();
+        List<QqchPostSetting> list = qqchPostSettingService.getTechDeptList();
+        for (int i = 0; i < list.size(); i++) {
+            QqchPostSetting temp = list.get(i);
+            temp.setProjectId(projectInfo.getProjectId());
+            temp.setProjectName(projectInfo.getProjectName());
+        }
+        return list;
     }
 }
