ce4e94e44556a9d7100c1f242c0b793326912cad
fushudong <10413614@qq.com>
2024-07-26 16:59:55 +0800

    bug

--- Ft-Cloud/ft-services/ft-service-sp/src/main/java/com/hhwy/sp/buildSchemeManage/sgjsBuildScheme/service/impl/SgjsBuildSchemeServiceImpl.java
+++ Ft-Cloud/ft-services/ft-service-sp/src/main/java/com/hhwy/sp/buildSchemeManage/sgjsBuildScheme/service/impl/SgjsBuildSchemeServiceImpl.java
@@ -433,13 +433,16 @@ public class SgjsBuildSchemeServiceImpl implements ISgjsBuildSchemeService {
         * 开关式  可关 可开
         */
         //如果打开状态，则发送
-        log.info("施工清单审批完成;开始发送提醒消息???  id：{}，isPass：{}", id,isPass);
-        if (StrUtil.isNotBlank(schemeListProcEndSwitch) && schemeListProcEndSwitch.trim().equals("on")){
+        log.info("施工清单审批完成;开始发送提醒消息???  id：{}，isPass：{}, schemeListProcEndSwitch: {}", id,isPass,schemeListProcEndSwitch);
+//        if (StrUtil.isNotBlank(schemeListProcEndSwitch) && schemeListProcEndSwitch.trim().equals("on")){
             this.sendProcessCompleteNotice(id, isPass);
-        }
+//        }
     }
 
     public void sendProcessCompleteNotice(Long id, String isPass) {
+        if (StrUtil.isNotBlank(schemeListProcEndSwitch) && schemeListProcEndSwitch.trim().equals("on")){
+            this.sendProcessCompleteNotice(id, isPass);
+        }
         log.info("施工清单审批完成;开始发送提醒消息!!!  id：{}，isPass：{}", id,isPass);
         //获取该流程第一、第二个节点审批人信息（发起人/区域中心技术负责人）
         List<Map<String, String>> flowHistoryInfo = FlowInfoSearchUtil.getFlowHistoryInfo(id, null);
