38e4b3836d6d37addaf05192880e40e944deffc9
fushudong <10413614@qq.com>
2024-07-26 17:33:34 +0800

    获取任务信息

--- Ft-Cloud/ft-services/ft-service-flowable/src/main/java/com/hhwy/flowable/controller/MyFlowController.java
+++ Ft-Cloud/ft-services/ft-service-flowable/src/main/java/com/hhwy/flowable/controller/MyFlowController.java
@@ -5,6 +5,8 @@ import com.hhwy.flowable.core.domain.TaskResource;
 import com.hhwy.flowable.core.service.IBpmnService;
 import com.hhwy.flowable.service.INodeTaskService;
 import org.apache.commons.lang3.ObjectUtils;
+import org.flowable.engine.HistoryService;
+import org.flowable.task.api.history.HistoricTaskInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +24,8 @@ public class MyFlowController {
     private INodeTaskService nodeTaskService;
     @Autowired
     private IBpmnService bpmnService;
+    @Autowired
+    private HistoryService historyService;
 
     @RequestMapping("/isNowfirstNode")
     public AjaxResult isNowfirstNode(@RequestParam("insId") String insId){
@@ -34,6 +38,14 @@ public class MyFlowController {
     public AjaxResult taskInfoDetail(@RequestParam("taskId") String taskId){
         TaskResource taskResource = bpmnService.taskInfoDetail(taskId);
         return AjaxResult.success(taskResource);
+    }
+
+
+    //获取任务信息
+    @RequestMapping("/ttt111111")
+    public AjaxResult taskInfoDetail1(@RequestParam("taskId") String taskId){
+        HistoricTaskInstance task = historyService.createHistoricTaskInstanceQuery().taskId(taskId).includeProcessVariables().singleResult();
+        return AjaxResult.success(task);
     }
 
 
